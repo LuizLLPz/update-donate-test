2c446832f775cb4b8393300020f654529164bf36
LuizLLPz <luizllp321@gmail.com>
2022-03-20 10:51:58 -0300

    Implementação inicial do cors

--- update-donate-api/src/index.js
+++ update-donate-api/src/index.js
@@ -1,9 +1,14 @@
 import App from "express";
 import { config } from "dotenv-safe";
+import cors from "cors";
 import { apiHandler } from "./api/master.js";
 
 async function main() {
     config();
+    corsOptions = {
+        origin: process.env.CONTENT_ORIGIN,
+        optionsSuccessStatus: 200
+    };
     const app = App();
     app.listen(process.env.SERVER_PORT, () => {
         console.log(`Server is running on port ${process.env.SERVER_PORT}`);
