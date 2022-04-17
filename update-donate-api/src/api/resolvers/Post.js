7aaae9cbbd5ba8bbe260ce1f4f0c1103470a1018
LuizLLPz <luizllp321@gmail.com>
2022-04-17 14:17:26 -0300

    Melhorando retorno da api

--- update-donate-api/src/api/resolvers/Post.js
+++ update-donate-api/src/api/resolvers/Post.js
@@ -1,18 +1,20 @@
 export default class Post {
     static get = async (req, res, conn) => {
-        return res.json(await conn.schema.raw(`SELECT p.id, p.titulo,p.categoria,p.corpo,p.created_at,p.pid, u.nome, COUNT(p2.pid) as respostas
+        let data = await conn.schema.raw(`SELECT p.id, p.titulo,p.categoria,p.corpo,p.created_at,p.pid, u.nome, COUNT(p2.pid) as respostas
         FROM
             Post p INNER JOIN User as u on u.id = p.uid LEFT JOIN
             Post p2 ON p.id = p2.pid
         where p.pid is NULL
         GROUP BY p.id
        
-        order by p.id desc;`));
+        order by p.id desc;`);
+        return res.json(data[0]);
     };
 
     static post = async (req, res, conn) => {
         if(req.body.pid) {
             const post =  await conn.select().from('Post').where({id: req.body.pid});
+            console.log(post)
             return res.json({post: post, respostas: 'resp'});
         }
 
